51a0b6e4670567c31f5d85e7a03a75476071dd18
emil <emil.raev@gmail.com>
2021-02-16 21:06:38 -0500

    easy 1 start

---
+++ Easy_1/exercises.js
@@ -0,0 +1,24 @@
+//1.
+
+function isOdd(num) {
+  return Boolean(num % 2);
+}
+// const isOdd = num => Boolean(num % 2);
+
+//2.
+
+// for (let num = 1; num <= 99; num += 2) {
+//   console.log(num);
+// }
+
+// function logOddInRange(start, end) {
+//   let current = start;
+//   while (current <= end) {
+//     if (current % 2) {
+//       console.log(current);
+//     }
+//     current += 1;
+//   }
+// }
+
+//4.
